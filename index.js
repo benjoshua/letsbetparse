a747bbc4d7424dde25ae7724f3994afec20d9969
Yoav Ben <yoavbenyehoshua@Yoavs-MacBook-Pro.local>
2016-02-22 12:14:11 +0200

    keys2

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '' //Add your master key here. Keep it secret!
+  appId: 'com.sashimi.letsbet',
+  masterKey: "d37e616e-61dc-4602-9afa-39c4f0fc6001" //Add your master key here. Keep it secret!
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
