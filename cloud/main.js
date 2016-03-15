e611b068c997478ee854dd6406da5946e2c32500
Popo Shadmi <tomshadmi@gmail.com>
2016-03-15 18:39:07 +0200

    piko

--- cloud/main.js
+++ cloud/main.js
@@ -316,7 +316,7 @@ Parse.Cloud.define("addGuessToFootballGameBet", function(request, response) {
 										response.error("couldn't find userID to add his guess");
 									}else{
 										console.log("6.7");
-										sendAdminMsgToGroup(layerGroupId, "" + user.get("name") +  " added a guess to bet");//, bet.id
+										sendAdminMsgToGroup(layerGroupId, "" + user.get("name") +  " added a guess to bet " + bet.id,{});
 										console.log("returning success");
 										response.success(true);
 									}
