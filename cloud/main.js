68f61316e7ea6a1abde69dc8eb412a3db5b216f3
Popo Shadmi <tomshadmi@gmail.com>
2016-03-30 14:31:24 +0300

    piko

--- cloud/main.js
+++ cloud/main.js
@@ -618,7 +618,7 @@ Parse.Cloud.define("getLBFootballMatchesBetweenDates", function(request, respons
 	var time = (14 * 24 * 3600 * 1000); // 14 days from today
 	var expirationDate = new Date(d.getTime() + (time));
 	//query.exists("date");
-	//query.equalTo("matchId","350048");
+	query.lessThanOrEqualTo("updatedAt",expirationDate);
 	query.find({
 		success: function(matches) {
 			console.log(matches);
