181d8a2c72a23d6c8c8da462e84513c1ef933784
Popo Shadmi <tomshadmi@gmail.com>
2016-04-14 00:16:54 +0300

    piko

--- cloud/main.js
+++ cloud/main.js
@@ -266,7 +266,7 @@ Parse.Cloud.define("createGroup", function(request, response) {
 				var newGroup = new LBGroupClass();
 				newGroup.set("layerGroupId",groupLayerId);
 				newGroup.set("groupAdminLayerId",groupAdminLayerId);
-				newGroup.set("statistics","");
+				newGroup.set("statistics","bla");
 				newGroup.save(null,{
 					success:function(newGroupSuccess) { 
 						console.log("created new group in db");
