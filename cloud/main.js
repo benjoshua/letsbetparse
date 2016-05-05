2206b15bd8ba6af158b3f3aac3ea7106324b75a8
Popo Shadmi <tomshadmi@gmail.com>
2016-05-05 03:12:11 +0300

    piko

--- cloud/main.js
+++ cloud/main.js
@@ -682,7 +682,7 @@ function addLBFootballMatchToDB(matchId, date, leagueId, homeTeam, homeTeamId, a
 // ------------------------- getLBFootballMatches ----------------------------
 //Get all LBFootballMatches saved in the DB
 Parse.Cloud.define("getLBFootballMatches", function(request, response) {
-	var LBFootballGameMatchlass = Parse.Object.extend("LBFootballMatch");
+	var LBFootballGameMatchClass = Parse.Object.extend("LBFootballMatch");
 	var query = new Parse.Query(LBFootballGameMatchClass);
 	query.find({
 		success: function(matches) {
