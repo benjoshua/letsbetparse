7ff0797548d368fd88fe520331babc813fb94f7d
Popo Shadmi <tomshadmi@gmail.com>
2016-05-24 12:19:45 +0300

    piko

--- cloud/main.js
+++ cloud/main.js
@@ -1104,7 +1104,6 @@ function updateBetsParticipatedStatForUser(userLayerId){
 		success: function(user) {
 			//If user exists in Parse:
 			if (user != undefined && user != null) {
-				console.log("34");
 				var amountOfBetsParticipated = user.get("betsParticipated");
 				amountOfBetsParticipated = amountOfBetsParticipated + 1;
 				user.set("betsParticipated",amountOfBetsParticipated);
@@ -1334,17 +1333,14 @@ Parse.Cloud.define("closeCustomBet", function(request, response) {
 	query.equalTo("_id",betId);
 	query.first({
 		success: function(bet) {
-			console.log("1");
 			//If bet doesn't exist in DB:
 			if ((bet == undefined) || (bet == null)) {
-				console.log("1");
 				response.error("bet wasn't found");
 			}else{
 				if (bet.get("betAdminLayerId") != userLayerId){
-					console.log("3");
+					
 					response.error("this user isn't an admin, thus can't close the bet");
 				}else{
-					console.log("4");
 					usersGuesses = bet.get("usersGuesses");
 					if (!(winningGuess in usersGuesses)){
 						response.error("winning guess wasn't even a possibility");
@@ -1353,17 +1349,13 @@ Parse.Cloud.define("closeCustomBet", function(request, response) {
 					
 					for (var guess in usersGuesses) {
 						if (usersGuesses.hasOwnProperty(guess)) {
-							console.log("5");
 							var usersArray = usersGuesses[guess];
 							//Someone guessed right
 							if (winningGuess === guess){
-								console.log("6");
 								for (var i = 0; i < usersArray.length; i++) {
-									console.log("9");
 									updateWinStatForUser(usersArray[i]);
 								}
 							}else{
-								console.log("7");
 								for (var i = 0; i < usersArray.length; i++) {
 									console.log("8");
 									updateBetsParticipatedStatForUser(usersArray[i]);
@@ -1482,7 +1474,7 @@ Parse.Cloud.define("getLastBetForGroup", function(request, response) {
 				console.log("2");
 				response.error("group wasn't found");
 			}else{
-				console.log("3");
+				console.log("3333");
 				JSON.stringify(group, null, 4);
 				var iko = group.get("lastBetId");
 				console.log("lastBetId: "+iko);
